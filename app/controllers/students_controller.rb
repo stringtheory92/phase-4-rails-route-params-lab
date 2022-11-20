17ce7ddd0786ee13ad5164998bd01b11477fea92
Adam Kabak <adamkabak@yahoo.com>
2022-11-20 07:55:35 -0500

    commit tree ef8a9ff7259158738843cf3f65297971bd5f891f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      
+      name = params[:name][0].upcase + params[:name].slice(1, params[:name].length)
+      students = Student.where(last_name: name)
+      
+    else
+      students = Student.all
+    end
+    
+
     render json: students
   end
+
+  def show
+    render json: Student.find(params[:id])
+  end
+
 
 end
